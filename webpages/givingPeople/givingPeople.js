6aa2b7d229ad96fad3ff356add1ca915c0161c1e
Talha Mustafa <91594421+TalhaMustafa1@users.noreply.github.com>
2023-11-24 03:28:19 +0500

    done today

---
+++ webpages/givingPeople/givingPeople.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    // Add click event listener to the tab link
+    $("#v-pills-link6-tab").on("click", function () {
+        // Jump to the element with ID "andreagassi"
+        window.location.href = "#andreagassi";
+    });
+});
+
